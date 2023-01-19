eec0151101d3f647b1967b42573f0291eaa194c2
Santiago gil <gilcorredorsantiago@gmail.com>
2023-01-19 18:30:08 -0500

    Primer comentario

---
+++ codigo.py
@@ -0,0 +1,15 @@
+persona = int(input("Digite su edad: "))
+nombre = input("Digite su nombre")
+if persona >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+def libros(a):
+    if a == 'Laura':
+        print("Hola Laura")
+    else:
+        print("No eres Laura")
+    return
+
+libros(a='jose')
