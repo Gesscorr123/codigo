7e2f996d144dd2343c315bbcd5dc5f892918af0d
Santiago Gil <gilcorredorsantiago@gmail.com>
2023-06-06 17:45:06 -0500

    Este es el primer commit de este archivo

---
+++ practica.py
@@ -0,0 +1,19 @@
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+n = int(input("Ingrese un número: "))
+contador = 0
+numero_actual = 2
+
+while contador < n:
+    if es_primo(numero_actual):
+        print(numero_actual)
+        contador += 1
+    numero_actual += 1
+
+print("hola mundo")
